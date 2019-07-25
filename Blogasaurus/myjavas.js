86aaf1149d201a9edf242ff49c9c25f3997e0226
RockyTides <RockyTides@users.noreply.github.com>
2019-07-24 23:07:09 -0400

    f

--- Blogasaurus/myjavas.js
+++ Blogasaurus/myjavas.js
@@ -1,11 +1,19 @@
-
-
-function hoverFunc(){
+function ImageMOFunc(){
+  var f = document.querySelector('#samoyed');
 
 }
 
+function hoverFunc(){
+  var f = document.querySelector('.c1');
+  f.style.color = "red";
+  f.style.font = "italic";
+  f.style.fontSize = "42px";
+}
+
 function exitFunc(){
-  
+  var f = document.querySelector('.c1');
+  f.style.color = "black";
+  f.style.fontSize = "32px"
 }
 
 function BlinkFunc(){
